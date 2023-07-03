ef413777139a0e2cafd31d4934f5a51e8f83f894
antor-mha <antordon500@gmail.com>
2023-07-03 21:19:13 +0600

    Simple Sort

--- uri_1042.c
+++ uri_1042.c
@@ -1,5 +1,33 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    int a,b,c;
+    int a, b, c, x, y, z, temp;
+    scanf("%d %d %d", &a, &b, &c);
+    x = a;
+    y = b;
+    z = c;
+
+    if (a > b)
+    {
+        temp = a;
+        a = b;
+        b = temp;
+    }
+    if (a > c)
+    {
+        temp = a;
+        a = c;
+        c = temp;
+    }
+    if (b > c)
+    {
+        temp = b;
+        b = c;
+        c = temp;
+    }
+
+    printf("%d\n%d\n%d\n\n", a, b, c);
+    printf("%d\n%d\n%d\n", x, y, z);
+
+    return 0;
 }
