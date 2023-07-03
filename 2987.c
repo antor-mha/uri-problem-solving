13f3649c28a2224cc7103fb6802bea4321d0cf78
antor-mha <antordon500@gmail.com>
2023-07-03 21:55:16 +0600

    Balloon of Honor

---
+++ 2987.c
@@ -0,0 +1,92 @@
+#include<stdio.h>
+int main()
+{
+    char L;
+    scanf("%c",&L);
+    switch (L)
+    {
+    case 'A':
+        printf("1\n");
+        break;
+    case 'B':
+        printf("2\n");
+        break;
+    case 'C':
+        printf("3\n");
+        break;
+    case 'D':
+        printf("4\n");
+        break;
+    case 'E':
+        printf("5\n");
+        break;
+    case 'F':
+        printf("6\n");
+        break;
+    case 'G':
+        printf("7\n");
+        break;
+    case 'H':
+        printf("8\n");
+        break;
+    case 'I':
+        printf("9\n");
+        break;
+    case 'J':
+        printf("10\n");
+        break;
+    case 'K':
+        printf("11\n");
+        break;
+    case 'L':
+        printf("12\n");
+        break;
+    case 'M':
+        printf("13\n");
+        break;
+    case 'N':
+        printf("14\n");
+        break;
+    case 'O':
+        printf("15\n");
+        break;
+    case 'P':
+        printf("16\n");
+        break;
+    case 'Q':
+        printf("17\n");
+        break;
+    case 'R':
+        printf("18\n");
+        break;
+    case 'S':
+        printf("19\n");
+        break;
+    case 'T':
+        printf("20\n");
+        break;
+    case 'U':
+        printf("21\n");
+        break;
+    case 'V':
+        printf("22\n");
+        break;
+    case 'W':
+        printf("23\n");
+        break;
+    case 'X':
+        printf("24\n");
+        break;
+    case 'Y':
+        printf("25\n");
+        break;
+    case 'Z':
+        printf("26\n");
+        break;
+
+    default:
+        break;
+    }
+
+    return 0;
+}
